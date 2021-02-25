39648cf80bf180d2e6249e5e9c50a7ce502feda4
Robert Sese <sese@github.com>
2021-02-25 00:14:48 -0600

    Get fish

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
-const express = require("express")
+const express = require('express')
+const axios = require('axios').default
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('hello world!')
+})
+
+app.get('/fish', async (req, res) => {
+  try {
+    const data = await axios.get('https://acnhapi.com/v1/fish')
+    let fish = '<ul>' 
+    for (f in data.data) {
+      fish += `<li>${f}</li>`
+    }
+    fish += '</ul>' 
+
+    res.send(fish)
+  } catch (err) {
+    console.log('error getting fish', err)
+  }
 })
 
 app.listen(port, () => {
