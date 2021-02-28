72973fead4f99a422f3b50d6aa824b04ab291e13
Robert Sese <sese@github.com>
2021-02-28 13:44:17 -0600

    Add nodemon and handlebars

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const path = require('path')
 const axios = require('axios').default
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+
+app.use(express.static(publicDirectoryPath))
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
 })
 
 app.get('/fish', async (req, res) => {
