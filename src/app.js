27275fbea1d6f75e41f2ba368db933371ba18b51
Robert Sese <sese@github.com>
2021-02-28 22:20:16 -0600

    Add sea creature views

--- src/app.js
+++ src/app.js
@@ -50,6 +50,38 @@ app.get('/fish/:id', async (req, res) => {
   }
 })
 
+app.get('/sea-creatures', async (req, res) => {
+  try {
+    const data = await axios.get('https://acnhapi.com/v1/sea')
+    let seaCreatures = '<ul>' 
+
+    for (f in data.data) {
+      seaCreatures += `<li><a href="/sea-creatures/${data.data[f].id}">${f}</a></li>`
+    }
+    seaCreatures += '</ul>' 
+
+    res.render('sea-creatures', {
+      seaCreatures: seaCreatures 
+    })
+  } catch (err) {
+    console.log('error getting all sea creatures', err)
+  }
+})
+
+app.get('/sea-creatures/:id', async (req, res) => {
+  try {
+    const data = await axios.get(`https://acnhapi.com/v1/sea/${req.params.id}`)
+
+    res.render('sea-creature', {
+      name: data.data['file-name'],
+      image_uri: data.data.image_uri,
+      museum_phrase: data.data['museum-phrase'],
+    })
+  } catch (err) {
+    console.log('error getting sea creature', err)
+  }
+})
+
 app.get('*', (req, res) => {
   res.render('404')
 })
