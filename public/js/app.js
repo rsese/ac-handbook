0c00b42397125e5c2e09c78cffb833c9139dc887
Robert Sese <sese@github.com>
2021-03-25 23:34:51 -0500

    Some responsive cleanup and hamburger time

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,44 @@
 const init = async function() {
   const newCommentField = document.querySelector('#new-comment-field')
-  const href = window.location.href
-  const thingId = href.slice(href.lastIndexOf('/') + 1)
+    if (newCommentField) {
 
-  document.querySelector('#save-comments-btn').addEventListener('click', async event => {
-    try {
-      const response = await fetch(`/fish/${thingId}/comments`, {
-        method: 'POST', 
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({comment: newCommentField.value})
-      })
+    const href = window.location.href
+    const thingId = href.slice(href.lastIndexOf('/') + 1)
 
-      const savedMsg = document.querySelector('.saved-msg')
-      savedMsg.classList.toggle('hide')
-      window.setTimeout(toggle, 1000)
+    document.querySelector('#save-comments-btn').addEventListener('click', async event => {
+      try {
+        const response = await fetch(`/fish/${thingId}/comments`, {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({comment: newCommentField.value})
+        })
 
-      // add new comment to comment list
-      const createdComment = await response.json()
-      const newComment = document.createElement('li')
-      const newCommentTimeStamp = document.createElement('span')
+        const savedMsg = document.querySelector('.saved-msg')
+        savedMsg.classList.toggle('hide')
+        window.setTimeout(toggle, 1000)
 
-      newCommentTimeStamp.classList.add('timeStamp')
-      newCommentTimeStamp.appendChild(document.createTextNode(createdComment.timeStamp))
-      newComment.appendChild(newCommentTimeStamp)
-      newComment.appendChild(document.createTextNode(createdComment.body))
-      document.querySelector('.comments').appendChild(newComment)
+        // add new comment to comment list
+        const createdComment = await response.json()
+        const newComment = document.createElement('li')
+        const newCommentTimeStamp = document.createElement('span')
 
-      newCommentField.value = ""
-    } catch (err) {
-      console.log(`error saving comment`, err)
-    }
+        newCommentTimeStamp.classList.add('timeStamp')
+        newCommentTimeStamp.appendChild(document.createTextNode(createdComment.timeStamp))
+        newComment.appendChild(newCommentTimeStamp)
+        newComment.appendChild(document.createTextNode(createdComment.body))
+        document.querySelector('.comments').appendChild(newComment)
+
+        newCommentField.value = ""
+      } catch (err) {
+        console.log(`error saving comment`, err)
+      }
+    })
+  }
+
+  document.querySelector('.site-nav-container .burger').addEventListener('click', event => {
+   event.target.nextElementSibling.classList.toggle('collapsed')
   })
 }
 
